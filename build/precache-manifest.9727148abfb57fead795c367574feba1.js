ee71feb622481b832bcadff047682e7aa6dd6366
Mark Tellez <mark.tellez@icloud.com>
2020-06-04 14:27:49 -0500

    production build ready

---
+++ build/precache-manifest.9727148abfb57fead795c367574feba1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16508af902825e6220faffa1f56e1dcf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8f926001b37a9727fab6",
+    "url": "/static/js/2.5212a51f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5212a51f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "744030a8dcab7bb33fcc",
+    "url": "/static/js/main.4c1d636b.chunk.js"
+  },
+  {
+    "revision": "a6f95836a57b9f427861",
+    "url": "/static/js/runtime-main.3917a7bd.js"
+  }
+]);
